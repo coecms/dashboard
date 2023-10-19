a655b7b90211646a64adab2471839b2adc05bde0
Dale Roberts <dsroberts@unimelb.edu.au>
2023-10-19 16:21:58 +1100

    Update alignTime a bit

--- src/util/data/alignTime.tsx
+++ src/util/data/alignTime.tsx
@@ -9,11 +9,13 @@ export function alignTime(intime: dayjs.Dayjs) {
     // 1230 and 1830 UTC. It doesn't matter if the data is half an hour or
     // so out of date, so adjust the timestamp to the nearest hour after
     // one of those times. Javascript modulo is broken
-    outtime = outtime.hour(Math.floor((((intime.utc().hour()-1)%24)+24)%24/6)*6+1);
-    if ( intime.minute() < 30 && [1,7,13,19].includes(outtime.hour()) ) {
-        // Before 0130, 0730, 1330 or 1930 UTC, we may not have new data yet, so go
+    if ( intime.minute() < 30 && [1,7,13,19].includes(intime.hour()) ) {
+        // Between 0100-0130, 0700-0730, 1300-1330 or 1900-1930 UTC, we may not have new data yet, so go
         // back to the previous point
         outtime = outtime.subtract(6,'hours');
+    } else {
+        // All other times, round it.
+        outtime = outtime.hour(Math.floor((((intime.utc().hour()-1)%24)+24)%24/6)*6+1);
     }
     outtime = outtime.set('minute',30).set('second',0).set('millisecond',0);
     return outtime;
